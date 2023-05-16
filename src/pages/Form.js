c9bb8d24317fc58a48528bc81d23d945c23cf975
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-05-16 20:39:24 +0700

    Content Update

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,9 +1,34 @@
+import {
+    Card,
+    Input,
+    Checkbox,
+    Button,
+    Typography,
+} from "@material-tailwind/react";
+
 import Footer from '../components/Footer';
 
 const Form = () => {
     return ( 
-        <div className="min-h-screen flex flex-col justify-between">
-                Form Page
+        <div className="min-h-screen flex flex-col justify-center">
+            <Card color="transparent" shadow={false}>
+                <Typography variant="h4" color="white" className="text-center">
+                    Silakan Berikan Penilaian Anda
+                </Typography>
+                <Typography color="white" className="mt-1 font-normal text-center">
+                    Informasi yang Anda berikan akan kami jaga
+                </Typography>
+                <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 mx-auto">
+                    <div className="mb-4 flex flex-col gap-6">
+                        <Input color="white" size="lg" label="Name" />
+                        <Input color="white" size="lg" label="Email" />
+                        <Input color="white" type="password" size="lg" label="Password" />
+                    </div>
+                    <Button className="mt-6" fullWidth>
+                        Submit
+                    </Button>
+                </form>
+                </Card>
             <Footer />
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+
+module.exports = withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -12,5 +16,5 @@ module.exports = {
     },
   },
   plugins: [],
-}
+})
 
