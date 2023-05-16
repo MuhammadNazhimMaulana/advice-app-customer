9c3adc25c2e1033924b5ac824031bb42cd47fb0c
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-05-16 19:34:41 +0700

    Preparing Pages

---
+++ src/pages/Form.js
@@ -0,0 +1,12 @@
+import Footer from '../components/Footer';
+
+const Form = () => {
+    return ( 
+        <div className="min-h-screen flex flex-col justify-between">
+                Form Page
+            <Footer />
+        </div>
+    );
+}
+ 
+export default Form;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="font-bold text-xl">
+            Halo Dunia
+        </div>
+    );
+}
+ 
+export default Home;
