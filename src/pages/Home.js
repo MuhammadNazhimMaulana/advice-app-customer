0503469dd6e860028a3fbcb69371d9511d6ce452
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-05-16 19:59:18 +0700

    Update On view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+    const navigate = useNavigate();
+
     return ( 
-        <div className="font-bold text-xl">
-            Halo Dunia
+        <div className="flex items-center justify-center h-screen">
+            <button onClick={() => navigate("/form")} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                Klik Disini
+            </button>
         </div>
     );
 }
