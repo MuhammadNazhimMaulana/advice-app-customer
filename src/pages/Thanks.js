93e7d0752fbb97fb7cf4b6dbbbca6ff71520a104
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-05-18 13:25:13 +0700

    Fixing Thanks Oage

--- src/pages/Thanks.js
+++ src/pages/Thanks.js
@@ -3,8 +3,9 @@ const Thanks = () => {
 
 
     return (
-        <div>
-            Anjay
+        <div className="flex flex-col items-center justify-center h-screen">
+            <span className="text-4xl text-center md:text-6xl lg:text-7xl">Terima Kasih</span>
+            <span className="text-lg md:text-xl lg:text-2xl text-center">Data yang Anda Kirimkan Telah Tercatat</span>
         </div>
     );
 }
