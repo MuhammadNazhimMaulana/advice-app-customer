d1aa4c01fc8156ace0424782fffcc3ffd103fb6c
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-05-23 20:31:12 +0700

    Update on Emoji

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -3,20 +3,48 @@ import { useState } from "react";
 const StarRating = () => {
     const [rating, setRating] = useState(0);
     const [hover, setHover] = useState(0);
+
+    // Emoji
+    let emo = [];
+
+    const emoji = (index) => {
+
+      // Finfing Emoji
+        if(index === 5){
+          emo.push(<span className="star text-4xl lg:text-7xl">&#128526;</span>)
+        }else if(index === 4){
+          
+          emo.push(<span className="star text-4xl lg:text-7xl">&#128537;</span>)
+        }else if(index === 3){
+          
+          emo.push(<span className="star text-4xl lg:text-7xl">&#128513;</span>)
+        }else if(index === 2){
+          
+          emo.push(<span className="star text-4xl lg:text-7xl">&#128557;</span>)
+        } else if(index === 1){
+          
+          emo.push(<span className="star text-4xl lg:text-7xl">&#128561;</span>)
+        }
+    };
+
     return (
       <div className="star-rating text-center">
         {[...Array(5)].map((star, index) => {
           index += 1;
+
+          // Calling Emoji Function
+          emoji(index)
+
           return (
             <button
               type="button"
               key={index}
-              className={index <= (hover || rating) ? "on" : "off"}
+              className={index === (hover || rating) ? "on" : "off"}
               onClick={() => setRating(index)}
               onMouseEnter={() => setHover(index)}
               onMouseLeave={() => setHover(rating)}
             >
-              <span className="star text-4xl lg:text-7xl">&#9733;</span>
+              {emo[index - 1]}
             </button>
           );
         })}
