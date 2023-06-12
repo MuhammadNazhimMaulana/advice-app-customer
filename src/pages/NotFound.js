ba7a8be296a45a5718ddca9efea1259710a83bdc
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-06-12 13:37:03 +0700

    UPdate View on Not Found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,21 @@
+import { useNavigate  } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
+
 const NotFound = () => {
+    // Navigate
+    const navigate = useNavigate();
+
     return ( 
         <div className="flex flex-col items-center justify-center h-screen">
-            <span className="text-4xl text-center md:text-6xl lg:text-7xl">Maaf</span>
-            <span className="text-lg md:text-xl lg:text-2xl text-center">Halaman yang Anda Cari Tidak Tersedia, Silakan kembali ke Halaman Sebelumnya</span>
+
+            {/* Image */}
+            <img className="w-52 h-52 md:h-64 md:w-64 lg:w-96 lg:h-96" src={process.env.PUBLIC_URL+"/Images/404.png"} alt="Not Found"/>
+
+            {/* Writing or Text */}
+            <span className="text-xs md:text-xl lg:text-2xl text-center mt-3">Halaman yang Anda Cari Tidak Tersedia</span>
+
+            {/* Button */}
+            <Button onClick={() => navigate(-1)} className="mt-6">Kembali</Button>
         </div>
      );
 }
